93148d2ded377464d00f05c4c2d44bfb88694de7
Kirk Cumins <cumiki@f-secure.com>
2019-11-14 18:09:44 +0000

    New Updates with mutability issues!

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,38 @@ use crate::room::{Room,Door};
 use crate::person::{Person};
 
 
-fn go(direction: &str, player: &mut Person, dungeon: &Vec<Room> ) {
-    let mut rooms = IntoIterator::into_iter(dungeon);
-    let curr_room = rooms.find(|&room| room.name == player.current_room).unwrap();
+fn go<'a> (direction: &str, &mut player: &mut Person<'a>, mut dungeon: &[room::Room<'a>; 4]) {
+    // let mut rooms = IntoIterator::into_iter(dungeon);
+    let curr_room = dungeon.iter().find(|room| room.name == player.current_room.name).unwrap();
     let destinations = curr_room.find_exits(direction);
+    let mut room_to_move = dungeon.iter_mut().find(|room| room.name == destinations).unwrap();
 
-    player.change_room(destinations.to_string());
+    player.change_room(room_to_move);
 
-    println!("You are in {}, You can go to {}",curr_room.doors[0].name, destinations);
+    println!("You are in {}, You can go to {}", player.current_room.name, destinations.to_string());
 
 }
 
 fn where_am_i(player: &Person) {
-    println!("You are in the {} room.", player.current_room);
+    println!("You are in the {} room.", player.current_room.name);
 }
 
 
 fn main() {
+    let dungeon = [
+        Room::new("North".to_string(), vec![Door::new("Forest","West"), Door::new("Desert","South")]),
+        Room::new("West".to_string(), vec![Door::new("Castle","East")]),
+        Room::new("East".to_string(), vec![Door::new("Desert","South")]),
+        Room::new("South".to_string(), vec![Door::new("Swamp","East"), Door::new("Forest","North")])
+        ];
     let mut name = String::new(); 
         println!("What is your name: ");
         io::stdin().read_line(&mut name)
             .ok()
             .expect("failed to read line");
-    let mut player = Person::new(name, "North".to_string());
+    let mut player = Person::new(name, &mut dungeon[0]);
     println!("Hi {}", player.name);
 
-    let mut dungeon = Vec::new();
-    dungeon.push(Room::new("North".to_string(), vec![Door::new("Forest","West"), Door::new("Desert","South")]));
-    dungeon.push(Room::new("West".to_string(), vec![Door::new("Castle","East")]));
-    dungeon.push(Room::new("East".to_string(), vec![Door::new("Desert","South")]));
-    dungeon.push(Room::new("South".to_string(), vec![Door::new("Swamp","East"), Door::new("Forest","North")]));
     loop {
         let mut command = String::new(); 
         println!("Enter your choice");
@@ -48,7 +50,7 @@ fn main() {
         let object = commands.get(1).unwrap_or(&"");
 
         match action.trim() {
-            "go" => go(object, &mut player, &dungeon),
+            "go" => go(object, &player, &dungeon),
             "where" => where_am_i(&player),
             "quit" => break,
             _ => println!("Cannot do that: {}", object),
--- src/person.rs
+++ src/person.rs
@@ -1,17 +1,19 @@
-pub struct Person{
+use crate::room::Room;
+
+pub struct Person<'a>{
     pub name: String,
-    pub current_room: String
+    pub current_room: &'a mut Room<'a>
 }
 
-impl Person{
-    pub fn new(name: String, room: String) -> Person {
+impl<'a> Person<'a>{
+    pub fn new(name: String, room: &'a mut Room<'a>) -> Person<'a> {
         Person {
             name: name,
             current_room: room,
         }
     }
 
-    pub fn change_room(&mut self, new_room: String){
+    pub fn change_room(&mut self, new_room: &'a mut Room<'a>){
         self.current_room = new_room;
     }
 }
